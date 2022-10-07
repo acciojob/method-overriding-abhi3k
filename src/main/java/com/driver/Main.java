b66be2d292b16d177c8ed3bdb6ee4027c4d37df8
abhi3k <abhisheks71@yahoo.com>
2022-10-07 23:43:54 +0530

    sol3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,15 +7,14 @@ public class Main {
   }
 
     public static class A {
-        public static void meth() {
-            System.out.println("Invoking method from class A");
+        public static String meth() {
+            return "Invoking method from class A";
         }
     }
 
     public static class B extends A{
-        public static void meth() {
-
-            System.out.println("Method is overridden in Extended class B");
+        public static String meth() {
+            return "Method is overridden in Extended class B";
         }
     }
 
